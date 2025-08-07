adc16d20e510b68e329d8e45147e79d7586b81ca
Yoshitha <yoshitha.kothapalli@sasi.ac.in>
2025-08-07 20:00:22 +0530

    Fixing GitHub Pages deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
-    port: 8080,
+    port: 0,
   },
   plugins: [
     react(),
